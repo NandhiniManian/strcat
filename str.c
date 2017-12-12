8fbb1c06656bde4bcc17f5dbf9f5a8c420763441
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2017-12-12 15:50:36 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[30],b[30];
+printf("Enter the two srings:"\n);
+gets(a);
+gets(b);
+printf("the two strings are:%s\n%s\n",a,b);
+for(i=0;a[i]!='\0';i++);
+for(j=0;b[j]!="\0";++j;++i)
+{
+a[i]=b[j];
+}
+a[i]!='\0';
+printf("combination of two string is:%s",a);
+}
